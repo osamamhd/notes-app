9a90562bb9be8ec7aad54f793a2ebb338ebd7301
Osama Mhd <osamamhd82@gmail.com>
2021-02-13 13:45:05 +0200

    create note model.

--- notes_app/note/models.py
+++ notes_app/note/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=220)
+    content = models.TextField()
+    slug = AutoSlugField("Note Titel", 
+        unique=True, always_update=False, populate_from="title")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
