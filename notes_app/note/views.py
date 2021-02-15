d7d9679bf65feb389a44bad6203bdb1d342643b8
Osama Mhd <osamamhd82@gmail.com>
2021-02-15 12:23:03 +0200

    add note detail view.

--- notes_app/note/views.py
+++ notes_app/note/views.py
@@ -1,7 +1,10 @@
-from  django.views.generic import ListView
+from  django.views.generic import ListView, DetailView
 
 
 from .models import Note
 
 class NoteListView(ListView):
+    model = Note
+
+class  NoteDetailView(DetailView):
     model = Note
