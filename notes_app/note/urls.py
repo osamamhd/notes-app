113997e58dd52a22b13737515b883bcdf65dd8b9
Osama Mhd <osamamhd82@gmail.com>
2021-02-15 12:22:44 +0200

    add path for note detail.

--- notes_app/note/urls.py
+++ notes_app/note/urls.py
@@ -6,6 +6,9 @@ app_name = "note"
 
 urlpatterns = [
     path(
-        route='/',view=views.NoteListView.as_view(), name='list'
+        route='',view=views.NoteListView.as_view(), name='list'
+    ),
+    path(
+        route='<slug:slug>/', view=views.NoteDetailView.as_view(), name='detail'
     ),
 ]
