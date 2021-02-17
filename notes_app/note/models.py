0d1721d9b7583c7cf999a6a82b77a15daf10c546
Osama Mhd <osamamhd82@gmail.com>
2021-02-17 15:13:32 +0200

    add color tuple in note.models.

--- notes_app/note/models.py
+++ notes_app/note/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from autoslug import AutoSlugField
 from django.urls import reverse
 
+
+COLORS = (
+    ('a', 'yellow'),
+    ('b', 'red'),
+    ('c', 'green'),
+    ('d', 'blue'),
+    ('e', 'indigo'),
+    ('f', 'purple'),
+    ('g', 'pink')
+)
 class Note(models.Model):
     title = models.CharField(max_length=220)
     content = models.TextField()
@@ -9,6 +19,7 @@ class Note(models.Model):
         unique=True, always_update=False, populate_from="title")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    color = models.CharField(max_length=1, choices=COLORS, default='a')
 
     def __str__(self):
         return self.title
